28df6e775bc543a395439cfe2fbda9c5d3cf9820
Pragyan Chaudhary <chaudharypragyan@gmail.com>
2020-12-21 20:56:22 +0545

    first commit

---
+++ database/migrations/2020_09_30_023521_create_questions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->boolean('published')->default(false);
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
